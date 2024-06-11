9245dbaaef256ee0ecb358c306229d51af94185f
Lester (Vivobook) <lestercheong70+github@outlook.com>
2024-06-12 03:13:06 +0800

    extracted zip

---
+++ src/test/java/com/cheonglol/whatever/WhateverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheonglol.whatever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhateverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
