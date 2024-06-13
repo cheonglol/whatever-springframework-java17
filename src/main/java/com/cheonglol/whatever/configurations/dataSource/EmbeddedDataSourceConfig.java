c1e14c086eb8c00856235fc1cfc29844b1764310
Lester (Vivobook) <lestercheong70+github@outlook.com>
2024-06-13 22:15:36 +0800

    configured embedded h2 db (im tired)

--- src/main/java/com/cheonglol/whatever/configurations/dataSource/EmbeddedDataSourceConfig.java
+++ src/main/java/com/cheonglol/whatever/configurations/dataSource/EmbeddedDataSourceConfig.java
@@ -1,10 +1,37 @@
-package com.cheonglol.whatever.configurations.dataSource;
+package com.cheonglol.whatever.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Profile("embedded-database")
+@EnableJpaRepositories(basePackages = "com.cheonglol.whatever")
 public class EmbeddedDataSourceConfig {
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    // Additional beans or configurations can be defined here
 }
