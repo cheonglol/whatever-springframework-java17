3816b2a3b0c409739c31d815e35aaa90153f24ef
Lester (Vivobook) <lestercheong70+github@outlook.com>
2024-07-11 01:32:58 +0800

    added `GenericResponseShape` to help with json response

---
+++ src/main/java/com/cheonglol/whatever/models/Response/GenericResponseShape.java
@@ -0,0 +1,57 @@
+package com.cheonglol.whatever.models.Response;
+
+import java.util.HashMap;
+
+public class GenericResponseShape {
+
+    private boolean success;
+    private Object data;
+    private String message;
+
+    public GenericResponseShape(boolean isSuccessful, Object data, String message) {
+
+        this.success = isSuccessful;
+        this.data = data;
+
+        if (message.equals(null) || message.isEmpty()) {
+            this.message = "";
+        } else {
+            this.message = message;
+        }
+
+    }
+
+    // GET SET SHIT
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // MAGIC SHIT
+    // public HashMap<String, Object> buildResponseHashMap() {
+    // return new HashMap<String, Object>() {{
+    // put("success", );
+    // put(message, data);
+    // put(message, data);
+    // }};
+    // }
+}
